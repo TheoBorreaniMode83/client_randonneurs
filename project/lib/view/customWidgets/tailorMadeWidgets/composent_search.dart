43b3b95a69803b1e20b432dc2a044272b7c1866d
TheoBorreaniMode83 <142791267+TheoBorreaniMode83@users.noreply.github.com>
2023-12-08 14:40:20 +0100

    creation du front de la recherche ùulti critères

--- project/lib/view/customWidgets/tailorMadeWidgets/composent_search.dart
+++ project/lib/view/customWidgets/tailorMadeWidgets/composent_search.dart
@@ -1,11 +1,10 @@
+
 import 'package:flutter/material.dart';
 
-import 'package:les_randonneurs_draceniens_client_administratif/view/customWidgets/standardWidgets/custom_dropdown_button.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/view/customWidgetsStyles/styles/standardWidgetsStyles/custom_dropdown_button_style.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/controller/customWidgetsControllers/custom_dropdown_button_content.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/controller/customWidgetsControllers/custom_dropdown_button_event.dart';
 
-import 'package:les_randonneurs_draceniens_client_administratif/view/customWidgets/standardWidgets/custom_text_form_field.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/view/customWidgetsStyles/styles/standardWidgetsStyles/custom_text_form_field_style.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/controller/customWidgetsControllers/custom_text_form_field_validator.dart';
 import 'package:les_randonneurs_draceniens_client_administratif/controller/customWidgetsControllers/custom_text_form_field_event.dart';
@@ -14,7 +13,14 @@ import 'package:les_randonneurs_draceniens_client_administratif/controller/custo
 import 'package:les_randonneurs_draceniens_client_administratif/view/customWidgetsStyles/styles_factory.dart';
 
 
-class ComposentSearch extends StatelessWidget{
+class ComposentSearch extends StatefulWidget {
+    const ComposentSearch ({Key? key});
+
+  @override
+  State<ComposentSearch> createState() => _ComposentSearchState();
+}
+
+class _ComposentSearchState extends State<ComposentSearch> {
 
   CustomDropdownButtonEvent customDropdownButtonEvent = CustomDropdownButtonEvent();
   CustomDropdownButtonStyle customDropdownButtonStyle = CustomDropdownButtonStyle();
@@ -27,11 +33,13 @@ class ComposentSearch extends StatelessWidget{
   CustomTextFormFieldContent customTextFormFieldContent = CustomTextFormFieldContent();
   final controller = TextEditingController();
 
+  bool switchVar = true;
 
 
 
-  ComposentSearch ({Key? key}){
-    
+
+  @override
+  void initState() {
     a = customDropdownButtonContent;
 
     customDropdownButtonEvent
@@ -46,176 +54,302 @@ class ComposentSearch extends StatelessWidget{
     customDropdownButtonContent
       .setItems(['>'])
       .setDropdownValue('>');
+    super.initState();
   }
 
+  Widget rowType1({required name,required List<String> tab1 ,required List<String> tab2}){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          Expanded(
+            flex:4,
+            child: Text(name),),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab1[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab1.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab2[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab2.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+        ]
+      )
+    );
+  }
 
+  Widget rowType2({required name,required List<String> tab1}){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          Expanded(
+            flex:4,
+            child: Text(name),),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab1[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab1.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+          Expanded(
+            flex:4,
+            child: Container(                   
+              color: Colors.white,
+              child: TextField()
+            ),
+          ),
+        ]
+      )
+    );
+  }
+
+  Widget rowType3({required name,required List<String> tab1, required List<String> tab2,required List<String> tab3}){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          Expanded(
+            flex:4,
+            child: Text(name),),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab1[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab1.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab2[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab2.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              color: Colors.white,
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: tab3[0],
+                onChanged:(value) => {},
+                alignment: Alignment.center,
+                items: tab3.map((value) => DropdownMenuItem(
+                  child:  Center( 
+                    child: Text(value), 
+                  ),
+                  value: value,
+                )).toList(),
+                icon: Container(
+                  height: double.maxFinite,
+                  width: 30,
+                  color:Colors.amber,
+                  child: const Icon(Icons.arrow_downward),
+                ),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.transparent,)
+              )
+            )
+          ),
+        ]
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      color: Colors.red,
-      child: Column(
-        children: [
-          Container(
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2
+    return  GestureDetector(
+      onTap: (){
+        setState(() {switchVar = !switchVar;});},
+      child:Container(
+        color: Colors.red,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2),),
+              width: double.maxFinite,
+              child: Row(
+                children: [
+                  Text("Requête multi critères"),
+                  Expanded(
+                    flex: 1,
+                    child: Container( 
+                      alignment: Alignment.centerRight, 
+                      child:Icon(Icons.south,),),),           
+                ],
               ),
             ),
-            child: Row(
-              children: [
-                Text("Requête multi critères"),
-                Expanded(
-                  flex: 1,
-                  child: Container( 
-                    alignment: Alignment.centerRight, 
-                    child:Icon(Icons.south,),
+            if(switchVar == false) ...[
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration:const BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 144, 144),
+                  border: Border(
+                    left: BorderSide( 
+                      color: Colors.black,
+                      width: 2,),
+                    right: BorderSide( 
+                      color: Colors.black,
+                      width: 2,),
+                    bottom: BorderSide(                  
+                      color: Colors.black,
+                      width: 2,
+                    ),
                   ),
-                )
-              ],
-            )
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            decoration:const BoxDecoration(
-              color: const Color.fromARGB(255, 255, 144, 144),
-              border: Border(
-                left: BorderSide( 
-                  color: Colors.black,
-                  width: 2,
-                  ),
-                right: BorderSide( 
-                  color: Colors.black,
-                  width: 2,
-                  ),
-                bottom: BorderSide(                  
-                  color: Colors.black,
-                  width: 2,
-                  ),
+                ),
+                child: Column(
+                  children: [
+                    rowType1(name: "Année de première adhésion", tab1:['=','>','<','≠'] , tab2: ['vide','b','c']),
+                    rowType1(name: "Civilité", tab1:['=','≠'] , tab2: ['vide','b','c']),
+                    rowType1(name: "Statut", tab1:['=','≠'] , tab2: ['vide','b','c']),
+                    rowType1(name: "Ville", tab1:['=','≠'] , tab2: ['vide','b','c']),
+                    rowType2(name: "Date de naissance", tab1:['=','≠','>','<']),
+                    rowType3(name: "Date de naissance", tab1:['=','≠','>','<'],tab2: ['vide','a','b'],tab3: ['vide','a','b','c']),
+                  ]
                 ),
               ),
-            child: Column(
-              children: [
-                //-------------------------------------------------------------------------------------
-                Row(
-                  children: [
-                    Expanded(
-                      flex:4,
-                      child: Text("Année de première adhésion : "),
-                    ),
-                    Flexible(
-                      flex: 2,
-                      child: Container(
-                        color: Colors.white,
-                        child: DropdownButton<String>(
-                          isExpanded: true,
-                          value: '<',
-                          onChanged:(value) => {},
-                          items: ['<','>','='].map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          icon: const Icon(Icons.arrow_downward),
-                          elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
-                          underline: Container(
-                            height: 2,
-                            color: Colors.transparent,
-                          )))),
-                    Expanded(
-                      flex: 10,
-                      child:Container(
-                      margin: EdgeInsets.symmetric(horizontal: 3),
-                      child: Container(
-                        color: Colors.white,
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },))))
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                      flex:1,
-                      child: Text("Année de première adhésion : "),
-                    )
-                  ],),
-                Row(
-                  children: [
-                    Flexible(
-                      flex:1,
-                      child: Text("Année de première adhésion : "),
-                    )
-                  ],),
-                //-----------------------------------------------------------------------
-              ],
-            )
-          )
-        ],
-      )
+            ],    
+          ],
+        ),
+      ),
     );
   }
 } 
 
-/*
-Container(
-                  margin: EdgeInsets.all(3),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center ,
-                    children: [
-                      Expanded(
-                        flex: 45,
-                        child: Text("Année de première adhésion : ")),
-                      Expanded(
-                        flex: 10,
-                        child: Container(
-                          color: Colors.white,
-                          child: DropdownButton<String>(
-                            value: '<',
-                            onChanged:(value) => {},
-                            items: ['<','>','='].map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          icon: const Icon(Icons.arrow_downward),
-                          elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
-                          underline: Container(
-                            height: 2,
-                            color: Colors.transparent,
-                          ) 
-                        )
-                      )
-                    ),
-                  Expanded(
-                    flex: 45,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 3),
-                      child: Container(
-                        color: Colors.white,
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        )
-                      )
-                    )
-                  )
-                ],
-              ),
-            ),
-*/
+
